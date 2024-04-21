a8f0d66b5d1e26dfbd44f8a6308fb441b927c70f
chelaroo <105634974+chelaroo@users.noreply.github.com>
2024-04-21 16:00:33 +0300

    Add functionalty

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,8 +7,9 @@
 import pandas as pd
 from currency_converter import CurrencyConverter
 import sys
+import pickle
 
-def preprocess_data(file_path, output_file):
+def preprocess_data(file_path, output_file, output_file_continue):
     # Read data from csv
     try:
         df = pd.read_csv(file_path, header=0)
@@ -16,7 +17,6 @@ def preprocess_data(file_path, output_file):
         raise FileNotFoundError("Error: File not found.")
 
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-    df.sort_values(by='Timestamp', inplace=True)
 
     # Add column for Unix time
     df['unix_timestamp'] = (df['Timestamp'] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
@@ -33,6 +33,39 @@ def preprocess_data(file_path, output_file):
     df['sender_uid'] = df['From Bank'].astype(str) + df['Account'].astype(str)
     df['receiver_uid'] = df['To Bank'].astype(str) + df['Account.1'].astype(str)
 
+    # Import the column transformer and the trained model
+    with open('column_transformer.pkl', 'rb') as f:
+        column_transformer = pickle.load(f)
+
+    with open('initial_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+    # Make a copy of the df to preprocess the data using column transformer
+    # we need df for sending data to cpp pattern detect
+    data = df.copy()
+    data = column_transformer.transform(data)
+
+    ### define function to deal with column tranformer prefixes
+    # Remove prefixes from column transformer
+    def remove_prefixes(dataframe):
+        prefixes = ['onehot__', 'target__', 'remainder__']
+        for prefix in prefixes:
+            dataframe.columns = [col.replace(prefix, '') for col in dataframe.columns]
+
+        return dataframe
+    
+    data = remove_prefixes(data)    
+
+    y_proba = model.predict_proba(data[model.booster_.feature_name()])
+    data['ypred'] = y_proba
+    df['ypred'] = y_proba
+
+    threshold = df['ypred'].quantile(0.8)
+    df = df[df['ypred'] >= threshold]
+    data = data[data['ypred'] >= threshold]
+
+
+ 
     currency_symbol_dict = {'Australian Dollar': 'AUD', 'Bitcoin': 'BTC', 'Brazil Real': 'BRL', 
                             'Canadian Dollar': 'CAD', 'Euro': 'EUR', 'Mexican Peso': 'MXN', 'Ruble': 'RUB', 
                             'Rupee': 'INR', 'Saudi Riyal': 'SAR', 'Shekel': 'ILS', 'Swiss Franc': 'CHF', 
@@ -59,6 +92,7 @@ def preprocess_data(file_path, output_file):
     # Add columns for normalized received and paid amounts in USD
     df['normalized_received_amount'] = df.apply(lambda row: convert_to_usd(row['Amount Received'], row['receiving_currency_symbol']), axis=1)
     df['normalized_paid_amount'] = df.apply(lambda row: convert_to_usd(row['Amount Paid'], row['paid_currency_symbol']), axis=1)
+    df.sort_values(by='Timestamp', inplace=True)
 
     df = df.drop(columns=['Timestamp', 'Account', 'Account.1'])
 
@@ -67,8 +101,9 @@ def preprocess_data(file_path, output_file):
 
     # Save selected data to CSV
     df[selected_columns].to_csv(output_file, index=False, header=False, sep=" ")
+    data.to_csv(output_file_continue, index=False, header=False)
 
-    return df
+    # return data
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
